993fd7e071cf62d86e23cf652275ec6f6c9b5ee8
Homarv <pierratmarvin3@gmail.com>
2020-05-06 21:02:59 +0200

    version de 2.1

--- TGP/config/routes.rb
+++ TGP/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get '/gossip/:id', to: 'static#gossip', as: 'gossip_id'
   get 'user/:user_entry' , to:'user#show'
   get 'home', to: 'static#home'
   get  'team' ,to: 'static#team'
   get  'contact', to: 'static#contact'
 
-  resources :gossips
+  resources :users
+  resources :cities
+  resources :gossips 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
